209326998f630ba2be4bd6f75c4d28974a640769
Jasmin Dedic <jdedic1999@gmail.com>
2022-08-03 13:52:17 +0200

    Added eventlistener and scaled images for performance

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 "use strict";
 
+let panels = document.querySelectorAll(".panel");
+
+
+for (let i = 0; i < panels.length; i++){
+    panels[i].addEventListener("click", () => {
+        removeActiveClasses();
+        panels[i].classList.add("active");
+        
+    })
+}
+
+function removeActiveClasses(){
+    for (let i = 0; i < panels.length; i++){
+        panels[i].classList.remove("active");
+    }
+}
